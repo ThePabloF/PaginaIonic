f9a590c117f0c1fcd65fa82f784462e5ea051a53
S3BASCH <sebaschuquiguanga@gmail.com>
2023-02-13 09:25:50 -0500

    Primer Push

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: 'This is an alert!',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
 
 }
+
